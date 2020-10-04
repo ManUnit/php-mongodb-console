4cef9c349e8d9435d9573ad79933c83174927de3
Anan P <hs1gab@gmail.com>
2020-10-04 20:00:23 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -7,9 +7,9 @@ return [
             'driver' => 'mongodb',
             'host' => env('MONGO_DB_HOST', 'oho'),
             'port' => env('MONGO_MONGO_DB_PORT', 27017),
-            'database' => env('MONGO_DB_DATABASE', 'shopping'),
-            'username' => env('MONGO_MONGO_DB_USERNAME', 'root'),
-            'password' => env('MONGO_DB_PASSWORD', 'richly'),
+            'database' => env('MONGO_DB_DATABASE', 'dummydb'),
+            'username' => env('MONGO_MONGO_DB_USERNAME', 'maradmin'),
+            'password' => env('MONGO_DB_PASSWORD', 'marpassword'),
             'options' => [     
                 'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
             ],
